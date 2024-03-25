94b339843997f7fbe1e1d065c55ac1afa977dcdb
bumang <umangabhattarai11@gmail.com>
2024-03-25 14:41:55 +0545

    feat: configure eslint prettier and husky

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,39 @@
+import path from 'path';
+
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: isProd ? 'export' : 'standalone',
+  basePath: isProd ? '/concept-ecomm' : '',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+      },
+    ],
+  },
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.resolve.modules.push(path.resolve('./'));
+
+    return config;
+  },
 };
 
 export default nextConfig;
