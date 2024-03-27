173a1c5d9349b63c1d799cc37bd6d0ee5f6bdfc5
bumang <umangabhattarai11@gmail.com>
2024-03-27 20:39:48 +0545

    feat: implement react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { ReactElement, ReactNode } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { NextPage } from 'next';
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -18,6 +20,19 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const fiveMinutesInMs = 5 * 60 * 1000;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: true,
+      refetchOnReconnect: true,
+      retry: false,
+      staleTime: fiveMinutesInMs,
+    },
+  },
+});
+
 export const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout || ((page) => page);
   const persister = persistStore(store);
@@ -25,8 +40,11 @@ export const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   return getLayout(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persister}>
-        <Component {...pageProps} />
-        <Toaster position="top-center" />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <Toaster position="top-center" />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </PersistGate>
     </Provider>
   );
